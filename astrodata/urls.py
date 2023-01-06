1a07f3bda38f4d603c02df339d4b919c6f2d0c47
Gurkirat Singh <tbhaxor@gmail.com>
2023-01-07 00:08:20 +0530

    add emission app logic

--- astrodata/urls.py
+++ astrodata/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ionization/', include('ionization.urls'))
+    path('ionization/', include('ionization.urls')),
+    path('emission/', include('emission.urls')),
 ]
