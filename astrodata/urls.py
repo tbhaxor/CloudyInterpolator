b9f97faddcf511727080f1bbfff2f5c86f9ef601
Gurkirat Singh <tbhaxor@gmail.com>
2023-01-10 19:15:46 +0530

    add navigation template on the / route

--- astrodata/urls.py
+++ astrodata/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='navigation.html')),
     path('admin/', admin.site.urls),
     path('ionization/', include('ionization.urls')),
     path('emission/', include('emission.urls')),
