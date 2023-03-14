1c681e5dc853de5468b452a62e1c31f0e7242e51
Gurkirat Singh <tbhaxor@gmail.com>
2023-03-14 16:00:05 +0530

    fix form and show errors

--- ionization/forms.py
+++ ionization/forms.py
@@ -1,78 +1,74 @@
 from django.core.exceptions import ValidationError
-from django.forms import ChoiceField, FloatField, Form, IntegerField
+from django.forms import FloatField, Form, IntegerField, TypedChoiceField
 from django.forms.widgets import NumberInput, Select
+from PyAstronomy.pyasl.asl.atomicNo import AtomicNo
 
 MODE_TYPES = (
     ('CIE', 'Collisional Ionisation Equilibrium (CIE)'),
     ('PIE', 'Photoionization Equilibrium (PIE)')
 )
 
-PART_TYPES = (
-    ('element', 'Element'),
+SPECIES_TYPES = (
+    ('all', 'All'),
+    ('electron', 'Electron'),
     ('ion', 'ION')
 )
+
+parmanu = AtomicNo()
+
+TATVAS = []
+for atn in range(1, 31):
+    TATVAS.append((atn, f"{parmanu.getElementName(atn)} ({parmanu.getElSymbol(atn)})"))
+TATVAS = tuple(TATVAS)
 
 
 class InterpolateForm(Form):
     nh = FloatField(required=True,
-                    label='Number of Hydrogen atoms',
-                    widget=NumberInput(attrs={
-                        'class': 'form-control',
-                    }))
+                    label='Number Density of Hydrogen',
+                    widget=NumberInput(attrs={'class': 'form-control'}))
 
-    mode = ChoiceField(choices=MODE_TYPES,
-                       required=True,
-                       initial=MODE_TYPES[0],
-                       label="Select Mode",
-                       widget=Select(attrs={
-                           'class': 'form-control'
-                       }))
+    mode = TypedChoiceField(choices=MODE_TYPES,
+                            required=True,
+                            initial=MODE_TYPES[0],
+                            label="Select Mode",
+                            widget=Select(attrs={'class': 'form-select'}))
 
     temperature = FloatField(required=True,
                              label='Temperature (in Kelvins)',
-                             widget=NumberInput(attrs={
-                                 'class': 'form-control'
-                             }))
+                             widget=NumberInput(attrs={'class': 'form-control'}))
 
     metallicity = FloatField(required=True,
                              label='Metallicity',
-                             widget=NumberInput(attrs={
-                                 'class': 'form-control'
-                             }))
+                             widget=NumberInput(attrs={'class': 'form-control'}))
 
     redshift = FloatField(required=True,
-                          label='Redshift factor',
-                          widget=NumberInput(attrs={
-                              'class': 'form-control'
-                          }))
+                          label='Redshift Factor',
+                          widget=NumberInput(attrs={'class': 'form-control'}))
 
-    element = IntegerField(required=True,
-                           min_value=1,
-                           max_value=30,
-                           label='Atomic Number of Element',
-                           widget=NumberInput(attrs={
-                               'class': 'form-control'
-                           }))
+    element = TypedChoiceField(coerce=int, required=True,
+                               label='Select Element',
+                               choices=TATVAS,
+                               initial=TATVAS[0],
+                               widget=Select(attrs={'class': 'form-select', }))
 
     ion = IntegerField(required=True, label='Ion Count',
                        min_value=1,
-                       widget=NumberInput(attrs={
-                           'class': 'form-control'
-                       }))
+                       widget=NumberInput(attrs={'class': 'form-control'}))
 
-    part_type = ChoiceField(required=True,
-                            choices=PART_TYPES,
-                            initial=PART_TYPES[0],
-                            label='Select Part Type',
-                            widget=Select(attrs={
-                                'class': 'form-control'
-                            }))
+    species_type = TypedChoiceField(required=True,
+                                    choices=SPECIES_TYPES,
+                                    initial=SPECIES_TYPES[1],
+                                    label='Select Species Type',
+                                    widget=Select(attrs={'class': 'form-select'}))
 
     def clean_ion(self):
         ion = self.cleaned_data['ion']
-        element = self.cleaned_data['element']
+        element = self.cleaned_data.get('element')
 
-        if ion > element:
-            raise ValidationError(f'Cannot exceed the element count.', code='GT_ELEMENT')
+        if element is None:
+            return
+
+        if ion > element+1:
+            raise ValidationError(f'Cannot exceed the element+1 count (here, {element+1}).', code='gt_element')
 
         return ion
