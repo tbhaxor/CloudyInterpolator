60fbea88abb562aee62bd16869e91e07b37e4de8
Gurkirat Singh <tbhaxor@gmail.com>
2023-03-29 23:43:00 +0530

    fix typo in email help_text

--- feedback/models.py
+++ feedback/models.py
@@ -8,30 +8,30 @@ class FeedbackModel(models.Model):
         null=False,
         blank=False,
         default=None,
-        verbose_name="Email Address",
-        help_text="This will help use to reach you when we launch a new version of the application",
+        verbose_name='Email Address',
+        help_text='This will help us to reach you when we launch a new version of the application',
     )
 
     name = models.CharField(
         max_length=128,
         null=False,
         default=None,
-        verbose_name="Full Name",
-        help_text="This is how we will address you in the email",
+        verbose_name='Full Name',
+        help_text='This is how we will address you in the email',
     )
 
     message = models.TextField(
         null=False,
         blank=False,
         default=None,
-        verbose_name="Message",
-        help_text="Provide the feedback here",
+        verbose_name='Message',
+        help_text='Provide the feedback here',
     )
 
     created_at = models.DateTimeField(
         null=False,
         blank=False,
-        verbose_name="Created At",
+        verbose_name='Created At',
         editable=False,
         auto_now_add=True,
     )
@@ -40,8 +40,8 @@ class FeedbackModel(models.Model):
         return f"{self.name}'s feedback"
 
     class Meta:
-        get_latest_by = "-created_at"
-        verbose_name = "Feedback Submission"
-        verbose_name_plural = f"{verbose_name}s"
+        get_latest_by = '-created_at'
+        verbose_name = 'Feedback Submission'
+        verbose_name_plural = f'{verbose_name}s'
 
     pass
