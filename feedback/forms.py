bcaa476b63a68414331aef5157373515d0969f68
Manjunath G <manjumjng2@gmail.com>
2023-04-09 14:35:08 +0530

    Updated Form classes and other UI

--- feedback/forms.py
+++ feedback/forms.py
@@ -9,9 +9,9 @@ class FeedbackForm(ModelForm):
         model = FeedbackModel
         fields = ['email', 'name', 'message']
         widgets = {
-            'name': TextInput(attrs={'class': 'form-control'}),
-            'email': EmailInput(attrs={'class': 'form-control'}),
-            'message': Textarea(attrs={'class': 'form-control'}),
+            'name': TextInput(attrs={'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}),
+            'email': EmailInput(attrs={'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}),
+            'message': Textarea(attrs={'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}),
         }
 
     pass
