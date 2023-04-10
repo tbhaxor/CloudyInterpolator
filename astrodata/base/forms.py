e3ab8e7d1c87ec7df2ff3fae652fbdcd5446f3d8
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-11 00:01:29 +0530

    use custom styles and fix labels

--- astrodata/base/forms.py
+++ astrodata/base/forms.py
@@ -9,21 +9,16 @@ MODE_TYPES = (
 
 
 class InterpolateForm(Form):
-    nH = FloatField(
-        required=True, label=format_html('Number Density of Hydrogen (cm<sup>-3</sup>)'),
-        widget=NumberInput(
-            attrs={
-                'class':
-                'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}))
+    nH = FloatField(required=True, label=format_html('Number Density of Hydrogen (cm<sup>-3</sup>)'),
+                    widget=NumberInput(attrs={'class': 'form-control'}))
 
-    mode = TypedChoiceField(choices=MODE_TYPES, required=True, initial=MODE_TYPES[0], label='Select Mode', widget=Select(attrs={
-                            'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}))
+    mode = TypedChoiceField(choices=MODE_TYPES, required=True, initial=MODE_TYPES[0],
+                            label='Select Mode', widget=Select(attrs={'class': 'form-select'}))
 
-    temperature = FloatField(required=True, label='Temperature (in Kelvins)', widget=NumberInput(attrs={
-                             'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}))
+    temperature = FloatField(required=True, label='Temperature (in Kelvins)',
+                             widget=NumberInput(attrs={'class': 'form-control'}))
 
-    metallicity = FloatField(required=True, label='Metallicity', widget=NumberInput(attrs={
-                             'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}))
+    metallicity = FloatField(required=True, label='Metallicity (Z<sub>&#8857;</sub>',
+                             widget=NumberInput(attrs={'class': 'form-control'}))
 
-    redshift = FloatField(required=True, label='Redshift Factor', widget=NumberInput(attrs={
-                          'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}))
+    redshift = FloatField(required=True, label='Cosmic Redshift', widget=NumberInput(attrs={'class': 'form-control'}))
