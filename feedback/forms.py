426bbd40c7541f5852d0d5a9b7f48efc8344da7a
Manjunath G <manjumjng2@gmail.com>
2023-04-11 02:18:07 +0530

    Removed attrs class for inputs and updated error class style for inputs

--- feedback/forms.py
+++ feedback/forms.py
@@ -9,9 +9,9 @@ class FeedbackForm(ModelForm):
         model = FeedbackModel
         fields = ['email', 'name', 'message']
         widgets = {
-            'name': TextInput(attrs={'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}),
-            'email': EmailInput(attrs={'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}),
-            'message': Textarea(attrs={'class': 'block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6'}),
+            'name': TextInput(),
+            'email': EmailInput(),
+            'message': Textarea(),
         }
 
     pass
