c44275665c2583ef85c7656b14d67821838e7e7f
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-13 04:29:11 +0530

    use the silent flag to prevent exit with error

--- deployment/management/commands/create-manager.py
+++ deployment/management/commands/create-manager.py
@@ -6,16 +6,21 @@ from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
-    help = "Closes the specified poll for voting"
+    help = 'Create the manager superuser account if does not exists.'
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--username",
+            '--username',
             help='Username to use for login (default: tbhaxor)',
             default='tbhaxor')
         parser.add_argument(
-            "--password",
+            '--password',
             help='Password for the admin account to use. If not passed, it will generate and print on stdout')
+        parser.add_argument(
+            '--silent',
+            default=False,
+            action='store_true',
+            help='Should not throw command error when set')
 
     def handle(self, *args, **options):
         password = options.get('password')
@@ -23,7 +28,11 @@ class Command(BaseCommand):
 
         user = User.objects.filter(username=username).first()
         if user is not None:
-            raise CommandError(f'Username {username} already exists.')
+            msg = f'Username {username} already exists.'
+            if options['silent']:
+                self.stdout.write(self.style.WARNING(f'{msg} Skipping....'))
+                return
+            raise CommandError(msg)
 
         if password is None:
             password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
