e1dfaf3d704f67b7eae3af6ed7b641cfae981974
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-13 04:31:15 +0530

    accept for the admin username in init script and default it to astrodata

--- deployment/scripts/django-init.bash
+++ deployment/scripts/django-init.bash
@@ -3,4 +3,5 @@
 set -ex
 
 ./manage.py migrate --no-input
+./manage.py create-manager --username="$ADMIN_USERNAME"
 gunicorn astrodata.wsgi:application -b "0.0.0.0:5000" --error-logfile - --access-logfile -
