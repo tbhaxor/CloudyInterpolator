3c5fb726d4779c60ea73e206974c7ce390837129
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-13 05:47:27 +0530

    add / in all the urls suffix

--- ionization/urls.py
+++ ionization/urls.py
@@ -4,6 +4,6 @@ from .views import DownloadFileView, InterpolationView
 
 app_name = 'ionization'
 urlpatterns = [
-    path('interpolation', InterpolationView.as_view(), name='interpolation'),
-    path('download/<int:batch_id>', DownloadFileView.as_view(), name='download'),
+    path('interpolation/', InterpolationView.as_view(), name='interpolation'),
+    path('download/<int:batch_id>/', DownloadFileView.as_view(), name='download'),
 ]
