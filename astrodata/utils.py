40d1796cae6a4dc98f632f86b7099a48ed9c10ed
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-14 10:10:39 +0530

    fix args len issue for completion

--- astrodata/utils.py
+++ astrodata/utils.py
@@ -3,12 +3,12 @@ import sys
 
 def is_server_running() -> bool:
     is_gunicorn = sys.argv[0].endswith('gunicorn')
-    is_dev = sys.argv[0].endswith('manage.py') and sys.argv[1] == 'runserver'
+    is_dev = sys.argv[0].endswith('manage.py') and len(sys.argv) > 1 and sys.argv[1] == 'runserver'
     return is_gunicorn or is_dev
 
 
 def is_test_running() -> bool:
-    return sys.argv[0].endswith('manage.py') and sys.argv[1] == 'test'
+    return sys.argv[0].endswith('manage.py') and len(sys.argv) > 1 and sys.argv[1] == 'test'
 
 
 def is_test_or_server_running() -> bool:
