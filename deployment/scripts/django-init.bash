670594075b7b868059bb10c9b22b864f48df998d
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-14 09:52:38 +0530

    set ADMIN_USERNAME environment variable to astrodata in django-init.bash

--- deployment/scripts/django-init.bash
+++ deployment/scripts/django-init.bash
@@ -3,5 +3,5 @@
 set -ex
 
 ./manage.py migrate --no-input
-./manage.py create-manager --username="$ADMIN_USERNAME" --silent
+./manage.py create-manager --username="${ADMIN_USERNAME:-astrodata}" --silent
 gunicorn astrodata.wsgi:application -b "0.0.0.0:5000" --error-logfile - --access-logfile -
