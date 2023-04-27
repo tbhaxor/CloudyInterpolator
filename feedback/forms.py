73db486d5862ef1ed9209324d1c775d8558261c8
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-27 17:52:39 +0530

    use one page for feedback

--- feedback/forms.py
+++ feedback/forms.py
@@ -4,6 +4,10 @@ from .models import FeedbackModel
 
 
 class FeedbackForm(ModelForm):
+    SESSION_KEY = "fdbk_form"
+
     class Meta:
         model = FeedbackModel
         fields = ["email", "name", "message"]
+
+        session_ignored_fields = ["message"]
