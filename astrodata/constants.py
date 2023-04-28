1c1b4fd0f4d2001413c87f2d2c7b87c362216f37
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-28 15:54:13 +0530

    add constants for ionization

--- astrodata/constants.py
+++ astrodata/constants.py
@@ -1,4 +1,5 @@
 from django.utils.html import format_html
+from PyAstronomy.pyasl.asl.atomicNo import AtomicNo
 
 # Enumeration / Choices
 MODE_TYPES = [
@@ -8,8 +9,28 @@ MODE_TYPES = [
 MODE_TYPES = sorted(MODE_TYPES, key=lambda x: x[0])
 MODE_TYPES = tuple(map(tuple, MODE_TYPES))
 
+SPECIES_TYPES = [
+    ["all", "All"],
+    ["electron", "Electron"],
+    ["ion", "ION"],
+]
+SPECIES_TYPES = sorted(SPECIES_TYPES, key=lambda x: x[0])
+SPECIES_TYPES = tuple(map(tuple, SPECIES_TYPES))
+PARMANU = AtomicNo()
+TATVAS = [(atn, f"{PARMANU.getElementName(atn)} ({PARMANU.getElSymbol(atn)})") for atn in range(1, 31)]
+
+
 # Labels
 NH_LABEL = format_html("Number Density of Hydrogen (cm<sup>-3</sup>)")
 TEMPERATURE_LABEL = format_html("Temperature (in Kelvins)")
+TEMPERATURE_RANGE_START_LABEL = format_html("Temperature Range Start (in Kelvins)")
+TEMPERATURE_RANGE_END_LABEL = format_html("Temperature Range End (in Kelvins)")
+TEMPERATURE_RANGE_STEP_LABEL = format_html("Temperature Range Step")
+SPECIES_TYPES_LABEL = format_html("Select Species Type")
 METALLICITY_LABEL = format_html("Metallicity (Z<sub>&#8857;</sub>)")
 REDSHIFT_LABEL = format_html("Cosmical Redshift")
+ELEMENT_LABEL = format_html("Select Element")
+ION_LABEL = format_html("Ion Count")
+
+# Session keys
+SESSION_FORM_DATA = "form_data"
