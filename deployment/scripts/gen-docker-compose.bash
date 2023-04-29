df121e4a0cc52252ee2874bfe0b8a6de401ba0b5
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-30 04:31:03 +0530

    add csrf trusted origins required in `gen-docker-compose.bash`

--- deployment/scripts/gen-docker-compose.bash
+++ deployment/scripts/gen-docker-compose.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 git_base_dir=$(git rev-parse --show-toplevel)
 ionization_dir=""
 emission_dir=""
 source_config="$git_base_dir/docker-compose.remote.yml"
 dest_config="$git_base_dir/docker-compose.yml"
+csrf_trusted_origins=""
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -25,15 +26,20 @@ while [[ $# -gt 0 ]]; do
             echo "[x] Source config file does not exist" >&2
         fi
         ;;
+    -c)
+        shift
+        csrf_trusted_origins="$1"
+        ;;
     -d)
         shift
         dest_config="$1"
         ;;
     *)
-        echo "usage: $0 -i DIR -e DIR [-s FILE] [-d FILE]"
+        echo "usage: $0 -i DIR -e DIR -c CSRF_TRUSTED_ORIGINS [-s FILE] [-d FILE]"
         echo "Arguments"
         echo -e "\t-i DIR\n\t\tIonization dataset directory"
         echo -e "\t-e DIR\n\t\tEmission dataset directory"
+        echo -e "\t-c FILE\n\t\tComma seprated list of origins for CSRF validation"
         echo -e "\t-s FILE\n\t\tSource docker-compose file (default: $source_config)"
         echo -e "\t-d FILE\n\t\tDestination docker-compose file (default: $dest_config)"
         exit 1
@@ -41,6 +47,13 @@ while [[ $# -gt 0 ]]; do
     esac
     shift
 done
+
+set -x
+
+if [[ -z "$csrf_trusted_origins" ]]; then
+    echo "[x] CSRF trusted origins are required" >&2
+    exit 1
+fi
 
 if [[ -z "$emission_dir" ]]; then
     echo "[x] Emission dataset directory required" >&2
@@ -65,5 +78,6 @@ if [[ $(uname) == "Darwin" ]]; then
     sed_args+=('')
 fi
 
-sed "${sed_args[@]}" "s|SOURCE_IONIZATION_DIR|$ionization_dir|g" "$dest_config"
-sed "${sed_args[@]}" "s|SOURCE_EMISSION_DIR|$emission_dir|g" "$dest_config"
+sed "${sed_args[@]}" "s|SOURCE_IONIZATION_DIR_PLACEHOLDER|$ionization_dir|g" "$dest_config"
+sed "${sed_args[@]}" "s|SOURCE_EMISSION_DIR_PLACEHOLDER|$emission_dir|g" "$dest_config"
+sed "${sed_args[@]}" "s|CSRF_TRUSTED_ORIGINS_PLACEHOLDER|$csrf_trusted_origins|g" "$dest_config"
